90152dc674c78e83b940da5d74a1262efbbbe00b
Yamac Tan <yamac.tan@turk.net>
2026-05-16 19:15:13 +0300

    Jekyll modified.

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,18 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: ({ name }) => {
+          const ext = name?.split('.').pop();
+          if (ext && ['css', 'js'].includes(ext)) {
+            return `assets/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+      },
+    },
   },
-})
+});
